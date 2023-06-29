bbdfebab7b6ad883ad89fd60933a89773ae7a577
KIM HYOJIN <oozxoocv82@gmail.com>
2023-06-29 12:40:40 +0900

    Delete project copy directory

--- project copy/js/background.js
+++
@@ -1,5 +0,0 @@
-const image=['0.jpeg','1.jpeg','2.jpeg'];
-const chosenImg=image[Math.floor(Math.random()*image.length)];
-
-// $('body').css('background-image',`url('img/${chosenImg}')`);
-$('body').attr('style',`background-image:url('img/${chosenImg}')`);
--- project copy/js/clock.js
+++
@@ -1,9 +0,0 @@
-const time = document.getElementById('clock');
-function getClock(){
-    const now = new Date();
-    const hour = String(now.getHours()).padStart(2,'0');
-    const minute = String(now.getMinutes()).padStart(2,'0');
-    const second = String(now.getSeconds()).padStart(2,'0');
-    time.innerHTML=`${hour}:${minute}:${second}`;
-}
-setInterval(getClock, 1000);
